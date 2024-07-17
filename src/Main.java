b7f05acfc33ae496bcfc67d8f8ab43ffd521e08c
Eugeny <goodwin.s@mail.ru>
2024-07-17 14:59:54 +0300

    create

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,27 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Ex1
+        System.out.println("Задача 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+        System.out.println();
+
+        //Ex2
+        System.out.println("Задача 2");
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+        System.out.println();
+
+        //Ex3
+        System.out.println("Задача 3");
+
+        System.out.println("Данные Ф. И. О. сотрудника - " + fullName.replace("ё", "е"));
+        System.out.println();
     }
 }
